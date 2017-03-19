dc3c8603cf3aadeb543234e1460044be3565b16f
Shmuela <shmuela@500tech.com>
2017-03-19 16:57:25 +0200

    auth component - implement smart component: inject auth service add methods add template

--- src/app/basics-demo/auth/auth.component.ts
+++ src/app/basics-demo/auth/auth.component.ts
@@ -1,19 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'sj-auth',
   template: `
+    <div>
+      <button (click)="signInWithGoogle()">Sign in with Google</button>
+    </div>
+    <div>
+      <input type="email" #emailInput/>
+      <input type="password" #passwordInput/>
+      <button (click)="signInWithEmail(emailInput.value, passwordInput.value)">
+        Sign in with email and password
+      </button>
+    </div>
+    <div>
+      <button (click)="signOut()">Sign Out</button>
+    </div>
+    
     <p>
-      auth works!
+      <label>Current authentication details:</label> {{ auth.authResponse | json }}
     </p>
+    <p>
+      <label>Current user details:</label> {{ auth.getUser() | json }}
+    </p>
+    <div>
+      <h3>Passing the user to stateless component</h3>
+      <sj-user-details [userDetails]="auth.getUser()"></sj-user-details>
+    </div>
   `,
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
+  }
+
+  signInWithGoogle() {
+    console.log('Google');
+  }
+
+  signInWithEmail(email, password) {
+    console.log(email, password);
+  }
+
+  signOut() {
+    console.log('sign out');
   }
 
 }
